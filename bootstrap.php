7c013f6bf0016a9cc944a1b86677a075245a1eba
wsydn <wsydney@web.de>
2022-09-02 10:37:51 +0200

    Drop PHP constant

--- bootstrap.php
+++ bootstrap.php
@@ -1,17 +1,11 @@
 <?php
 
 
-// Set path constants
-use craft\helpers\App;
-
 define('CRAFT_BASE_PATH', __DIR__);
 define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH . '/vendor');
 
 // Load Composer's autoloader
 require_once CRAFT_VENDOR_PATH . '/autoload.php';
-
-
-define('CRAFT_ENVIRONMENT', App::env('CRAFT_ENVIRONMENT') ?: 'production');
 
 // Load dotenv?
 if (class_exists(Dotenv\Dotenv::class)) {

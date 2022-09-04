e74eb9e08bdf8930d681d1601f00d25114ddee61
wsydn <wsydney@web.de>
2022-09-04 12:16:01 +0200

    Drop downs in primary nav

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,9 +15,11 @@ window.baguetteBox = baguetteBox;
 // Alpine JS
 import Alpine from 'alpinejs'
 import collapse from '@alpinejs/collapse'
+import focus from '@alpinejs/focus'
 
 window.Alpine = Alpine
 Alpine.plugin(collapse)
+Alpine.plugin(focus)
 Alpine.start()
 
 

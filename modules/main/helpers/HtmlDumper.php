9fc8b6b1bb41676b66a6c108b5a3777410a08451
wsydn <wsydney@web.de>
2023-01-04 18:27:16 +0100

    Dumper

---
+++ modules/main/helpers/HtmlDumper.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace modules\main\helpers;
+
+
+class HtmlDumper extends \Symfony\Component\VarDumper\Dumper\HtmlDumper
+{
+    protected static $themes = [
+        'light' => [
+            'default' => 'background:none; color:#CC7832; line-height:1.2em; font:18px Menlo, Monaco, Consolas, monospace; word-wrap: break-word; white-space: pre-wrap; position:relative; z-index:99999; word-break: break-all',
+            'num' => 'font-weight:bold; color:#1299DA',
+            'const' => 'font-weight:bold',
+            'str' => 'font-weight:bold; color:#629755;',
+            'note' => 'color:#6897BB',
+            'ref' => 'color:#6E6E6E',
+            'public' => 'color:#262626',
+            'protected' => 'color:#262626',
+            'private' => 'color:#262626',
+            'meta' => 'color:#B729D9',
+            'key' => 'color:#789339',
+            'index' => 'color:#1299DA',
+            'ellipsis' => 'color:#CC7832',
+            'ns' => 'user-select:none;',
+        ],
+    ];
+}
--- modules/main/twigextensions/TwigExtension.php
+++ modules/main/twigextensions/TwigExtension.php
@@ -3,8 +3,10 @@
 namespace modules\main\twigextensions;
 
 use Craft;
+use Symfony\Component\VarDumper\Cloner\VarCloner;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use Twig\TwigFunction;
 
 class TwigExtension extends AbstractExtension
 {
@@ -19,9 +21,40 @@ class TwigExtension extends AbstractExtension
         ];
     }
 
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('d', fn(mixed $var, int $depth = 20) => $this->dumpFunction($var, $depth)),
+
+            new TwigFunction('dd', fn(mixed $var, int $depth = 20) => $this->dumpAndDieFunction($var, $depth)),
+        ];
+    }
+
+
     public function quoteFilter(?string $text): string
     {
         return Craft::t('site', '“') . $text . Craft::t('site', '”');
+    }
+
+    public function dumpFunction(mixed $var, int $depth)
+    {
+        $dumper = new \modules\main\helpers\HtmlDumper();
+        $dumper->setTheme('light');
+        $dumper->dump((new VarCloner())->cloneVar($var)->withMaxDepth($depth));
+    }
+
+    public function dumpAndDieFunction(mixed $var, int $depth) {
+        while (ob_get_length() !== false) {
+            // If ob_start() didn't have the PHP_OUTPUT_HANDLER_CLEANABLE flag, ob_get_clean() will cause a PHP notice
+            // and return false.
+            if (@ob_get_clean() === false) {
+                break;
+            }
+        }
+
+        $this->dumpFunction($var, $depth);
+
+        exit();
     }
 
 }
